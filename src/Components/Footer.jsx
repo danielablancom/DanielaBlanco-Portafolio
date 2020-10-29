3ed4f177e2a4ad47b2a3c1a8fd883679b75319f6
labp02@labp02-Latitude-E6230 <blancodanielac@gmail.com>
2020-10-28 22:12:40 -0300

    responsive desktop, tablet and mobile

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Components/Footer.css';
+
+
+const Footer = () => {
+    return (
+        <div className="container-footer">
+             <div className="text-footer">
+             <p className="">
+             @Daniela Blanco, 2020
+            </p>
+            </div>
+           
+        </div>
+    );
+}
+
+export default Footer; 
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import '../Projects/Projects.css';
-import DataLovers from '../../img/DataLovers.jpg';
 import Buttons from '../Buttons';
+import dataLovers from '../../img/dataLovers.jpg';
+import burgerQueen2 from '../../img/burgerQueen2.jpg';
+import comidaparatodos from '../../img/comidaparatodos.jpg';
 
 const Projects = () => {
     return (
@@ -9,23 +11,25 @@ const Projects = () => {
             <div className="title-projects">
                 <p>Proyectos Realizados</p>
             </div>
-            <div className="container-img1">
-                <img src={DataLovers} className="img-logo" alt={"img-logo"}></img>
+            <div className="box-projects"> 
+            <div className="container-img-projects">
+                <img src={dataLovers} className="img-projects" alt={"data-lovers"}></img>
             </div>
             <div className="container-title-project">
 
-                <p className="project-name">Data Lovers</p>
+                <p className="project-name">Wizards Unite / Data Lovers</p>
 
             </div>
             <div className="container-title-project">
 
-                <p className="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p className="project-description">Wizards Unite es una App Web basada en el juego que lleva el mismo nombre donde usuarios pueden hacer búsquedas en tiempo real de sus personajes favoritos, filtrar y ordenar los datos, ya sea por orden alfabético o género. </p>
                 <Buttons />
             </div>
+            </div>
             
-           
-            <div className="container-img2">
-                <img src={DataLovers} className="img-logo" alt={"img-logo"}></img>
+            <div className="box-projects"> 
+            <div className="container-img-projects">
+                <img src={burgerQueen2} className="img-projects" alt={"burgerQueen"}></img>
             </div>
             <div className="container-title-project">
 
@@ -34,13 +38,15 @@ const Projects = () => {
             </div>
             <div className="container-title-project">
 
-                <p className="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p className="project-description">Burger Queen es un software gastronómico de comandas para restaurantes. El diseño y desarrollo se realizó con JavaScript, CSS, Reactjs y se almacenó la información en Firebase.</p>
                 <Buttons />
             </div>
             
-           
-            <div className="container-img3">
-                <img src={DataLovers} className="img-logo" alt={"img-logo"}></img>
+            </div>
+            
+            <div className="box-projects">
+            <div className="container-img-projects">
+                <img src={comidaparatodos} className="img-projects" alt={"comidaparatodos"}></img>
             </div>
             <div className="container-title-project">
 
@@ -49,8 +55,9 @@ const Projects = () => {
             </div>
             <div className="container-title-project">
 
-                <p className="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p className="project-description">Comida para todos en una fundación sin fines de lucro que busca salvaguardar la seguridad alimentaria en Chile. Se desarrolló re diseño de plataforma web utilizando React, JavaScript y Sass.</p>
                 <Buttons />
+            </div>
             </div>
             
         </div>
